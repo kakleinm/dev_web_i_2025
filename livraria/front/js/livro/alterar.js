d31305054aa2e680610a2692f644f91fdc7fdbef
KARENKM <karenkmaia09@gmail.com>
2025-11-27 17:32:47 -0300

    livraria

--- livraria/front/js/livro/alterar.js
+++ livraria/front/js/livro/alterar.js
@@ -1,28 +1,6 @@
 const btnAlterarLivro = document.querySelector("#alterarLivro")
 
 /**
- * Função construtora de Exemplares da Biblioteca
- * @param {*} issn 
- * @param {*} titulo 
- * @param {*} autor 
- * @param {*} editora 
- * @param {*} ano 
- * @param {*} genero 
- * @param {*} local 
- * @param {*} disponibilidade 
- */
-function Livro(ISSN, titulo, autor, editora, anoPublicacao, genero, localizacao, disponibilidade) {
-  this.ISSN = ISSN
-  this.titulo = titulo
-  this.autor = autor
-  this.editora = editora
-  this.anoPublicacao = anoPublicacao
-  this.genero = genero
-  this.localizacao = localizacao
-  this.disponivel = disponibilidade
-}
-
-/**
  * Função que deverá pegar os dados do formulário e gerar um novo 
  * exemplar na lista de livros da biblioteca
 */
